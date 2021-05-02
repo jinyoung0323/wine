7c602d4c342d94cfb940fd0889138ffb93cb4971
bongbong <bongbong@DESKTOP-7E2C6V2>
2021-05-02 21:55:05 +0900

    그럭저럭 메인에서 jsp는 뜨지만...

---
+++ src/main/java/com/javaex/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.javaex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.repository.MainDao;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private MainDao dao;
+	
+	@RequestMapping(value="/")
+	public String main() {
+
+		return "main/index";
+		
+		
+	}
+}
---
+++ src/main/java/com/javaex/repository/MainDao.java
@@ -0,0 +1,12 @@
+package com.javaex.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MainDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+}
